0e1cc984efd3da347b94008b8a5b59393a1de2fb
Ishan Arora <ishanarora04@gmail.com>
2021-03-21 16:54:45 +0530

    Counting Sort

---
+++ lib/sorting/counting_sort.rb
@@ -0,0 +1,38 @@
+require 'byebug'
+
+module Sorting
+    class CountingSort
+        
+        attr_reader :array
+
+        def initialize(array: nil)
+            @array = array
+        end
+
+        def sort
+
+            min = @array.min
+            max = @array.max
+            diff = max - min + 1
+            temp = [0] * diff
+
+            for elem in @array do            
+                temp[elem-min] += 1 
+            end
+
+            output = []
+            for elem in 0..temp.length do
+                for count in 1..temp[elem].to_i do
+                    output.append(elem + min)
+                end
+            end
+
+            for index in 0..@array.length do
+                @array[index] = output[index]
+            end
+
+        end
+
+
+    end
+end
---
+++ test/sorting/counting_sort_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative '../../lib/sorting/counting_sort'
+
+class CountingSortTest < Minitest::Test
+    
+    def test_array_sorts
+        is = Sorting::CountingSort.new(array: [3, 2, 1, 100, 200, 11, 22, 3, 4, 5])
+        is.sort()
+        assert_equal is.array.first(3), [1, 2, 3]
+    end
+
+end    
