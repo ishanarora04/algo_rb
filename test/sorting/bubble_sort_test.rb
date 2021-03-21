69c3255dc8f5608cec51224a22d2b8d20c23dc62
Ishan Arora <ishanarora04@gmail.com>
2021-03-21 16:18:21 +0530

    Bubble Sort

---
+++ test/sorting/bubble_sort_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative '../../lib/sorting/bubble_sort'
+
+class MergeSortTest < Minitest::Test
+    
+    def test_array_sorts
+        is = Sorting::BubbleSort.new(array: [3, 2, 1, 100, 200, 21000, 11001,1100, 110000, 11, 22,3,4,5])
+        is.sort()
+        assert_equal is.array.first(3), [1, 2, 3]
+    end
+
+end    
