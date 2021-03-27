7914e9f9afb33b999defadc3b6de21d3d10c9374
Ishan Arora <ishanarora04@gmail.com>
2021-03-27 18:54:12 +0530

    DP

---
+++ dp/matrix_chain_multiplication.rb
@@ -0,0 +1,16 @@
+module DP
+    class MatrixChainMultiplication 
+        
+        attr_reader :matrix
+
+        def initialize(matrix: nil)
+            @matrix = matrix
+            @length = matrix.length
+        end
+
+        def multiply
+
+        end
+
+    end
+end
